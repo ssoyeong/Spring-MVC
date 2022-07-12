e7931ecbd42da8fe4cb73ab7078d31b7faefe973
ssoyeong <62373980+ssoyeong@users.noreply.github.com>
2022-07-12 15:01:12 +0900

    Feat: View 분리 MyView 객체를 통해 해당 jsp 실행

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    // 각 컨트롤러에서 jsp를 부르는 것이 아니라 MyView를 거쳐서 렌더링
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
