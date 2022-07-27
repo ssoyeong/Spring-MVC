ecbb5ff68d6f76e3cc53b930f5467069906c8ccc
ssoyeong <62373980+ssoyeong@users.noreply.github.com>
2022-07-27 11:32:34 +0900

    Feat: 상품 상세 Controller 및 View 개발 각 상품의 상세정보 확인 기능

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,20 +21,18 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    /*
-    @RequiredArgsConstructor를 사용하면 해당 생성자 생략 가능
-
-    @Autowired    // 생성자가 하나면 생략 가능
-    public BasicItemController(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
-    }
-    */
-
     @GetMapping
     public String item(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     /*
