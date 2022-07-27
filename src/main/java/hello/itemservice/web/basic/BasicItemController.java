2bb38fb2a4cfe2e7e441b0dfffe8053b560d29e2
ssoyeong <62373980+ssoyeong@users.noreply.github.com>
2022-07-27 11:01:20 +0900

    Feat: BasicItemController 생성 및 View Template 적용 Thymeleaf를 사용하여 정적 items.html을 동적으로 변경

---
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,49 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    /*
+    @RequiredArgsConstructor를 사용하면 해당 생성자 생략 가능
+
+    @Autowired    // 생성자가 하나면 생략 가능
+    public BasicItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    */
+
+    @GetMapping
+    public String item(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /*
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+
+    }
+
+}
